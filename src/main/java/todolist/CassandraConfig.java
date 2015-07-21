c28232a1f3a902bda46196c881867f950820c0e4
Johannes Pieringer <JohannesPieringer@yan.int.consol.de>
2015-07-21 17:44:09 +0200

    initial

---
+++ src/main/java/todolist/CassandraConfig.java
@@ -0,0 +1,23 @@
+package todolist;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.cassandra.config.java.AbstractCassandraConfiguration;
+import org.springframework.data.cassandra.repository.config.EnableCassandraRepositories;
+
+/**
+ * Created by Johannes Pieringer on 20.07.2015.
+ */
+@Configuration
+@EnableCassandraRepositories(basePackages = { "todolist.repository" })
+public class CassandraConfig extends AbstractCassandraConfiguration {
+
+    @Override
+    public String getKeyspaceName() {
+        return "todolist";
+    }
+
+    @Override
+    protected String getContactPoints() {
+        return System.getProperty("DB_HOST");
+    }
+}
